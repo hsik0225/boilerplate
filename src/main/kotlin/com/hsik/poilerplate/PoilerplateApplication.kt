c3210c99a3da092d7c34a95b105ee3c99cd4de75
rowan <sk16013@eadevsvcskcorp.onmicrosoft.com>
2024-06-30 17:14:19 +0900

    Init Project

---
+++ src/main/kotlin/com/hsik/poilerplate/PoilerplateApplication.kt
@@ -0,0 +1,11 @@
+package com.hsik.poilerplate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PoilerplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<PoilerplateApplication>(*args)
+}
